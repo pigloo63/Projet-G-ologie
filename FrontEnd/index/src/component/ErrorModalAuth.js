db3104c7be03447f0a5e0241693a0a452ed8cc00
simon <simon.mandereau@gmail.com>
2023-01-25 16:14:39 +0100

    Ajout ErrorModal pour indiquer si le mot de passe est bon ou non

---
+++ FrontEnd/index/src/component/ErrorModalAuth.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const ErrorModalAuth = ({ closeWindow, close }) => {
+  return (
+    <div>
+      {close && (
+        <div className="fixed top-1/4 right-1/2 border bg-white rounded-3xl w-auto p-4">
+          <div>
+            <p>Message d'erreur</p>
+          </div>
+          <div>
+            <p>Mot de passe et/ou identifiant est incorrecte</p>
+          </div>
+          <div className="mt-5">
+            <button onClick={closeWindow}>Fermer</button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ErrorModalAuth;
